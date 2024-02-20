f30b871ea631a7b75b3da4ff543b9dea96dc8f76
Ahmed-coder143 <wajeed5924@gmail.com>
2024-02-20 15:32:31 +0530

    Rock

---
+++ Rock-Paper-Scissors Game.py
@@ -0,0 +1,12 @@
+P="P"
+R="R"
+S="S"
+x,y=input().split()
+if x==P and y==P or x==R and y==R or x==S and y==S:
+    print("D")
+elif x==P and y==R or x==R and y==P:
+     print("P")
+elif x==R and y==S or x==S and y==R:
+     print("R")
+else:
+    print("S")
