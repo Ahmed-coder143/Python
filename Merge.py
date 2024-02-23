df9af34be76299c191ad5dfd683fcef4f75c484a
Ahmed-coder143 <wajeed5924@gmail.com>
2024-02-23 19:11:41 +0530

    Merge

---
+++ Merge.py
@@ -0,0 +1,4 @@
+arr1=list(map(int,(input("Enter the array1 numbers: ").split())))
+arr2=list(map(int,(input("ENter the array2 numbers: ").split())))
+value=arr1+arr2
+print("Merged and Sorted Array: " ,*sorted(value))
