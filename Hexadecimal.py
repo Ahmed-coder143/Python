77da77512fcaa422715e4d655a1b132e6d580cdb
Ahmed-coder143 <wajeed5924@gmail.com>
2024-02-23 18:39:10 +0530

    Hexadecimal

---
+++ Hexadecimal.py
@@ -0,0 +1,4 @@
+Binary_number = input("Enter a binary number: ")
+Decimal_number = int(Binary_number, 2)
+Hexadecimal_number = hex(Decimal_number)[2:]
+print(f"The hexadecimal equivalent of {Binary_number} is: {Hexadecimal_number}")
