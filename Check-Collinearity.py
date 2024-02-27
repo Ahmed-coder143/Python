923c261adf9dbe1ed65d4c6593d8237a67abe90e
Ahmed-coder143 <wajeed5924@gmail.com>
2024-02-27 21:37:42 +0530

    check

--- Check-Collinearity.py
+++ Check-Collinearity.py
@@ -1,20 +1,20 @@
-# Input
+
 points = []
 for _ in range(3):
-    x, y = map(int, input().split())
+    x, y = map(int, input("Enter the numbers for three points: ").split())
     points.append((x, y))
 
-# Check if points lie on the same line
+
 x1, y1 = points[0]
 x2, y2 = points[1]
 x3, y3 = points[2]
 
-# Check if slopes are equal
+
 slope1 = (y2 - y1) * (x3 - x2)
 slope2 = (y3 - y2) * (x2 - x1)
 
-# Output
+
 if slope1 == slope2:
-    print('yes')
+    print("The points are collinear: yes")
 else:
-    print('no')
+    print("The points are non-collinear: no")
